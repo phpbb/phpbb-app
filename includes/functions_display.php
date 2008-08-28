phpbb/phpbb-app

commit 1379b317431885f7632d3c11c88fea601256a6b8

#29335
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@8749 89ea8834-ac86-4346-8a33-228a782c2dd0

includes/functions_display.php
@@ -1169,7 +1169,7 @@ function get_user_avatar($avatar, $avatar_type, $avatar_width, $avatar_height, $
 	}
 
 	$avatar_img .= $avatar;
-	return '<img src="' . $avatar_img . '" width="' . $avatar_width . '" height="' . $avatar_height . '" alt="' . ((!empty($user->lang[$alt])) ? $user->lang[$alt] : $alt) . '" />';
+	return '<img src="' . urlencode($avatar_img) . '" width="' . $avatar_width . '" height="' . $avatar_height . '" alt="' . ((!empty($user->lang[$alt])) ? $user->lang[$alt] : $alt) . '" />';
 }
 
 ?>

commit 3f2bdb2df81e371827fcb02701fc554572e92eb0

fixing fault introduced with R8749
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@8790 89ea8834-ac86-4346-8a33-228a782c2dd0

includes/functions_display.php
@@ -1183,7 +1183,7 @@ function get_user_avatar($avatar, $avatar_type, $avatar_width, $avatar_height, $
 	}
 
 	$avatar_img .= $avatar;
-	return '<img src="' . urlencode($avatar_img) . '" width="' . $avatar_width . '" height="' . $avatar_height . '" alt="' . ((!empty($user->lang[$alt])) ? $user->lang[$alt] : $alt) . '" />';
+	return '<img src="' . (str_replace(' ', '%20', $avatar_img) . '" width="' . $avatar_width . '" height="' . $avatar_height . '" alt="' . ((!empty($user->lang[$alt])) ? $user->lang[$alt] : $alt) . '" />';
 }
 
 ?>
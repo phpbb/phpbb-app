phpbb/phpbb-app

commit 10d72d3d0a7c1dccb9317c74faa9076f5e909ed7

Minor security problem, discovered internally. Requires the user to know the activation key which is not normally possible when admin activation is turned on. #41625
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9498 89ea8834-ac86-4346-8a33-228a782c2dd0

includes/ucp/ucp_activate.php
@@ -56,6 +56,17 @@ class ucp_activate
 			trigger_error('WRONG_ACTIVATION');
 		}
 
+		// Do not allow activating by non administrators when admin activation is on
+		// Only activation type the user should be able to do is INACTIVE_REMIND
+		if ($user_row['user_inactive_reason'] != INACTIVE_REMIND && $config['require_activation'] == USER_ACTIVATION_ADMIN && !$auth->acl_get('a_user'))
+		{
+			if (!$user->data['is_registered'])
+			{
+				login_box('', $user->lang['NO_AUTH_OPERATION']);
+			}
+			trigger_error('NO_AUTH_OPERATION');
+		}
+
 		$update_password = ($user_row['user_newpasswd']) ? true : false;
 
 		if ($update_password)

commit 2524ad243d3e317e7c259dc32f6a15af87d50280

Fix a regression in r9498, activation could really do with an overhaul :(
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9520 89ea8834-ac86-4346-8a33-228a782c2dd0

includes/ucp/ucp_activate.php
@@ -58,7 +58,8 @@ class ucp_activate
 
 		// Do not allow activating by non administrators when admin activation is on
 		// Only activation type the user should be able to do is INACTIVE_REMIND
-		if ($user_row['user_inactive_reason'] != INACTIVE_REMIND && $config['require_activation'] == USER_ACTIVATION_ADMIN && !$auth->acl_get('a_user'))
+		// or activate a new password which is not an activation state :@
+		if (!$user_row['user_newpasswd'] && $user_row['user_inactive_reason'] != INACTIVE_REMIND && $config['require_activation'] == USER_ACTIVATION_ADMIN && !$auth->acl_get('a_user'))
 		{
 			if (!$user->data['is_registered'])
 			{
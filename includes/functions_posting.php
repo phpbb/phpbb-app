phpbb/phpbb-app

commit aabb761113a7aa30066b49128b00fd7ef61b67fc

Fix bug #48265 - Correctly set attachment flag for topics, posts and pms after deleting attachments - Patch by WorldWar and nickvergessen
Authorised by: AcydBurn


git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9843 89ea8834-ac86-4346-8a33-228a782c2dd0

includes/functions_posting.php
@@ -1549,6 +1549,21 @@ function delete_post($forum_id, $topic_id, $post_id, &$data)
 		break;
 	}
 
+	if (($post_mode == 'delete') || ($post_mode == 'delete_last_post') || ($post_mode == 'delete_first_post'))
+	{
+		$sql = 'SELECT 1 AS has_attachments
+			FROM ' . ATTACHMENTS_TABLE . '
+			WHERE ' . $db->sql_in_set('topic_id', $topic_ids);
+		$result = $db->sql_query($sql);
+		$has_attachments = (int) $db->sql_fetchfield('has_attachments');
+		$db->sql_freeresult($result);
+
+		if (!$has_attachments)
+		{
+			$sql_data[TOPICS_TABLE] .= ', topic_attachment = 0';
+		}
+	}
+
 //	$sql_data[USERS_TABLE] = ($data['post_postcount']) ? 'user_posts = user_posts - 1' : '';
 
 	$db->sql_transaction('begin');

includes/functions_admin.php
@@ -892,19 +892,61 @@ function delete_attachments($mode, $ids, $resync = true)
 	// Update post indicators for posts now no longer having attachments
 	if (sizeof($post_ids))
 	{
-		$sql = 'UPDATE ' . POSTS_TABLE . '
-			SET post_attachment = 0
-			WHERE ' . $db->sql_in_set('post_id', $post_ids);
-		$db->sql_query($sql);
+		// Just check which posts are still having an assigned attachment not orphaned by querying the attachments table
+		$sql = 'SELECT post_msg_id
+			FROM ' . ATTACHMENTS_TABLE . '
+			WHERE ' . $db->sql_in_set('post_msg_id', $post_ids) . '
+				AND in_message = 0
+				AND is_orphan = 0';
+		$result = $db->sql_query($sql);
+
+		$remaining_ids = array();
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$remaining_ids[] = $row['post_msg_id'];
+		}
+		$db->sql_freeresult($result);
+
+		// Now only unset those ids remaining
+		$post_ids = array_diff($post_ids, $remaining_ids);
+
+		if (sizeof($post_ids))
+		{
+			$sql = 'UPDATE ' . POSTS_TABLE . '
+				SET post_attachment = 0
+				WHERE ' . $db->sql_in_set('post_id', $post_ids);
+			$db->sql_query($sql);
+		}
 	}
 
 	// Update message table if messages are affected
 	if (sizeof($message_ids))
 	{
-		$sql = 'UPDATE ' . PRIVMSGS_TABLE . '
-			SET message_attachment = 0
-			WHERE ' . $db->sql_in_set('msg_id', $message_ids);
-		$db->sql_query($sql);
+		// Just check which messages are still having an assigned attachment not orphaned by querying the attachments table
+		$sql = 'SELECT post_msg_id
+			FROM ' . ATTACHMENTS_TABLE . '
+			WHERE ' . $db->sql_in_set('post_msg_id', $message_ids) . '
+				AND in_message = 1
+				AND is_orphan = 0';
+		$result = $db->sql_query($sql);
+
+		$remaining_ids = array();
+		while ($row = $db->sql_fetchrow($result))
+		{
+			$remaining_ids[] = $row['post_msg_id'];
+		}
+		$db->sql_freeresult($result);
+
+		// Now only unset those ids remaining
+		$message_ids = array_diff($message_ids, $remaining_ids);
+
+		if (sizeof($message_ids))
+		{
+			$sql = 'UPDATE ' . PRIVMSGS_TABLE . '
+				SET message_attachment = 0
+				WHERE ' . $db->sql_in_set('msg_id', $message_ids);
+			$db->sql_query($sql);
+		}
 	}
 
 	// Now update the topics. This is a bit trickier, because there could be posts still having attachments within the topic

includes/message_parser.php
@@ -1334,7 +1334,7 @@ class parse_message extends bbcode_firstpass
 	/**
 	* Parse Attachments
 	*/
-	function parse_attachments($form_name, $mode, $forum_id, $submit, $preview, $refresh, $is_message = false)
+	function parse_attachments($form_name, $mode, $forum_id, $submit, $preview, $refresh, $is_message = false, $post_msg_id = 0, $topic_id = 0)
 	{
 		global $config, $auth, $user, $phpbb_root_path, $phpEx, $db;
 
@@ -1487,16 +1487,25 @@ class parse_message extends bbcode_firstpass
 							'filesize'			=> $filedata['filesize'],
 							'filetime'			=> $filedata['filetime'],
 							'thumbnail'			=> $filedata['thumbnail'],
-							'is_orphan'			=> 1,
+							'is_orphan'			=> ($post_msg_id) ? 0 : 1,
 							'in_message'		=> ($is_message) ? 1 : 0,
 							'poster_id'			=> $user->data['user_id'],
 						);
+
+						if ($post_msg_id)
+						{
+							$sql_ary['post_msg_id'] = $post_msg_id;
+							if ($topic_id)
+							{
+								$sql_ary['topic_id'] = $topic_id;
+							}
+						}
 
 						$db->sql_query('INSERT INTO ' . ATTACHMENTS_TABLE . ' ' . $db->sql_build_array('INSERT', $sql_ary));
 
 						$new_entry = array(
 							'attach_id'		=> $db->sql_nextid(),
-							'is_orphan'		=> 1,
+							'is_orphan'		=> ($post_msg_id) ? 0 : 1,
 							'real_filename'	=> $filedata['real_filename'],
 							'attach_comment'=> $this->filename_data['filecomment'],
 						);

includes/ucp/ucp_pm_compose.php
@@ -665,7 +665,22 @@ function compose_pm($id, $mode, $action)
 		}
 
 		// Parse Attachments - before checksum is calculated
-		$message_parser->parse_attachments('fileupload', $action, 0, $submit, $preview, $refresh, true);
+		if ($action == 'edit')
+		{
+			$message_parser->parse_attachments('fileupload', $action, 0, $submit, $preview, $refresh, true, $msg_id);
+			if (sizeof($message_parser->attachment_data))
+			{
+				// Update attachment indicators for pms having attachments now, as a precaution if the pm does not get stored by submit
+				$sql = 'UPDATE ' . PRIVMSGS_TABLE . '
+					SET message_attachment = 1
+					WHERE msg_id = ' . $msg_id;
+				$db->sql_query($sql);
+			}
+		}
+		else
+		{
+			$message_parser->parse_attachments('fileupload', $action, 0, $submit, $preview, $refresh, true);
+		}
 
 		if (sizeof($message_parser->warn_msg) && !($remove_u || $remove_g || $add_to || $add_bcc))
 		{

posting.php
@@ -700,7 +700,28 @@ if ($submit || $preview || $refresh)
 	}
 
 	// Parse Attachments - before checksum is calculated
-	$message_parser->parse_attachments('fileupload', $mode, $forum_id, $submit, $preview, $refresh);
+	if ($mode == 'edit')
+	{
+		$message_parser->parse_attachments('fileupload', $mode, $forum_id, $submit, $preview, $refresh, false, $post_id, $topic_id);
+		if (sizeof($message_parser->attachment_data))
+		{
+			// Update attachment indicators for post/topic having attachments now, as a precaution if the post does not get stored by submit
+			$sql = 'UPDATE ' . POSTS_TABLE . '
+				SET post_attachment = 1
+				WHERE post_id = ' . $post_id;
+			$db->sql_query($sql);
+
+			$sql = 'UPDATE ' . TOPICS_TABLE . '
+				SET topic_attachment = 1
+				WHERE topic_id = ' . $topic_id;
+			$db->sql_query($sql);
+		}
+	}
+	else
+	{
+		$message_parser->parse_attachments('fileupload', $mode, $forum_id, $submit, $preview, $refresh);
+	}
+
 
 	// Grab md5 'checksum' of new message
 	$message_md5 = md5($message_parser->message);

commit 13b4a248efb5c0328caae3df15f1a32f6082652f

Fix bug #48885, introduced in r9843
Authorised by: kellanved

git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9893 89ea8834-ac86-4346-8a33-228a782c2dd0

includes/functions_posting.php
@@ -1553,8 +1553,8 @@ function delete_post($forum_id, $topic_id, $post_id, &$data)
 	{
 		$sql = 'SELECT 1 AS has_attachments
 			FROM ' . ATTACHMENTS_TABLE . '
-			WHERE ' . $db->sql_in_set('topic_id', $topic_ids);
-		$result = $db->sql_query($sql);
+			WHERE topic_id = ' . $topic_id;
+		$result = $db->sql_query_limit($sql, 1);
 		$has_attachments = (int) $db->sql_fetchfield('has_attachments');
 		$db->sql_freeresult($result);
 
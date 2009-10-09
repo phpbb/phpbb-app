phpbb/phpbb-app

commit 0ffa7b498ba65dbc49ba37f508a2156c98549ff4

Preserve newlines in template files (one newline had been always dropped after a template variable due to PHP's handling of closing tags)
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9811 89ea8834-ac86-4346-8a33-228a782c2dd0

includes/template.php
@@ -82,6 +82,8 @@ class template
 		$this->root = $template_path;
 		$this->cachepath = $phpbb_root_path . 'cache/ctpl_' . str_replace('_', '-', $template_name) . '_';
 
+		$this->_rootref = &$this->_tpldata['.'][0];
+
 		return true;
 	}
 

includes/functions_template.php
@@ -251,15 +251,22 @@ class template_compile
 			$template_php .= (!$no_echo) ? (($trim_check_text != '') ? $text_blocks[$i] : '') . ((isset($compile_blocks[$i])) ? $compile_blocks[$i] : '') : (($trim_check_text != '') ? $text_blocks[$i] : '') . ((isset($compile_blocks[$i])) ? $compile_blocks[$i] : '');
 		}
 
+		// Remove unused opening/closing tags
+		$template_php = str_replace(' ?><?php ', ' ', $template_php);
+
+		// Now add a newline after each php closing tag which already has a newline
+		// PHP itself strips a newline if a closing tag is used (this is documented behaviour) and it is mostly not intended by style authors to remove newlines
+		$template_php = preg_replace('#\?\>([\r\n])#', '?>\1\1', $template_php);
+
 		// There will be a number of occasions where we switch into and out of
 		// PHP mode instantaneously. Rather than "burden" the parser with this
 		// we'll strip out such occurences, minimising such switching
 		if ($no_echo)
 		{
-			return "\$$echo_var .= '" . str_replace(' ?><?php ', ' ', $template_php) . "'";
+			return "\$$echo_var .= '" . $template_php . "'";
 		}
 
-		return str_replace(' ?><?php ', ' ', $template_php);
+		return $template_php;
 	}
 
 	/**

commit 48d69d71d6877dbb638f334a7044afc9d10c82ad

fix bug #52605 - regression from r9811
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10217 89ea8834-ac86-4346-8a33-228a782c2dd0

includes/template.php
@@ -152,6 +152,7 @@ class template
 	function destroy()
 	{
 		$this->_tpldata = array('.' => array(0 => array()));
+		$this->_rootref = &$this->_tpldata['.'][0];
 	}
 
 	/**
phpbb/phpbb-app

commit 719552a7e6389a1917089bf2dcf9555447a9a013

Fix bugs #46615 & #46945 - Fail gracefully if store folder is not writable during update.
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@9768 89ea8834-ac86-4346-8a33-228a782c2dd0

install/install_update.php
@@ -915,6 +915,11 @@ class install_update extends module
 				// Now init the connection
 				if ($update_mode == 'download')
 				{
+					if (function_exists('phpbb_is_writable') && !phpbb_is_writable($phpbb_root_path . 'store/'))
+					{
+						trigger_error(sprintf($user->lang['DIRECTORY_NOT_WRITABLE'], $phpbb_root_path . 'store/'), E_USER_ERROR);
+					}
+
 					if ($use_method == '.zip')
 					{
 						$compress = new compress_zip('w', $phpbb_root_path . 'store/' . $archive_filename . $use_method);

includes/functions_compress.php
@@ -155,7 +155,12 @@ class compress_zip extends compress
 	*/
 	function compress_zip($mode, $file)
 	{
-		return $this->fp = @fopen($file, $mode . 'b');
+		$this->fp = @fopen($file, $mode . 'b');
+
+		if (!$this->fp)
+		{
+			trigger_error('Unable to open file ' . $file . ' [' . $mode . 'b]');
+		}
 	}
 
 	/**

commit 06d46889eda6a18601685ba75ae3088461103cfd

Fix Bug #53965, Regression introduced in r9768 - Hardcode language for the time being.
git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10271 89ea8834-ac86-4346-8a33-228a782c2dd0

install/install_update.php
@@ -927,7 +927,7 @@ class install_update extends module
 				{
 					if (function_exists('phpbb_is_writable') && !phpbb_is_writable($phpbb_root_path . 'store/'))
 					{
-						trigger_error(sprintf($user->lang['DIRECTORY_NOT_WRITABLE'], $phpbb_root_path . 'store/'), E_USER_ERROR);
+						trigger_error(sprintf('The directory “%s” is not writable.', $phpbb_root_path . 'store/'), E_USER_ERROR);
 					}
 
 					if ($use_method == '.zip')
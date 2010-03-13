phpbb/phpbb-app

commit 493df9f8ebb5f8a723a4ee5ee85cc0133c9e5e20

Bug #57795 - [Fix] Restrict search for language/../iso.txt to folders.
Authorised by: bantu


git-svn-id: file:///svn/phpbb/branches/phpBB-3_0_0@10496 89ea8834-ac86-4346-8a33-228a782c2dd0

includes/acp/acp_language.php
@@ -1120,6 +1120,11 @@ class acp_language
 		{
 			while (($file = readdir($dp)) !== false)
 			{
+				if (!is_dir($phpbb_root_path . 'language/' . $file))
+				{
+					continue;
+				}
+
 				if ($file[0] != '.' && file_exists("{$phpbb_root_path}language/$file/iso.txt"))
 				{
 					if (!in_array($file, $installed))

commit 2a70318895de144f25678f719537880bf4b1b13a

[bug/59135] Fix open_basedir issues when accessing styles- and language-management. (Bug #59135) introduced in r10496 and r10360/r10361

includes/acp/acp_language.php
@@ -1120,12 +1120,12 @@ class acp_language
 		{
 			while (($file = readdir($dp)) !== false)
 			{
-				if (!is_dir($phpbb_root_path . 'language/' . $file))
+				if ($file[0] == '.' || !is_dir($phpbb_root_path . 'language/' . $file))
 				{
 					continue;
 				}
 
-				if ($file[0] != '.' && file_exists("{$phpbb_root_path}language/$file/iso.txt"))
+				if (file_exists("{$phpbb_root_path}language/$file/iso.txt"))
 				{
 					if (!in_array($file, $installed))
 					{

includes/acp/acp_styles.php
@@ -643,13 +643,13 @@ parse_css_file = {PARSE_CSS_FILE}
 		{
 			while (($file = readdir($dp)) !== false)
 			{
-				if (!is_dir($phpbb_root_path . 'styles/' . $file))
+				if ($file[0] == '.' || !is_dir($phpbb_root_path . 'styles/' . $file))
 				{
 					continue;
 				}
 
 				$subpath = ($mode != 'style') ? "$mode/" : '';
-				if ($file[0] != '.' && file_exists("{$phpbb_root_path}styles/$file/$subpath$mode.cfg"))
+				if (file_exists("{$phpbb_root_path}styles/$file/$subpath$mode.cfg"))
 				{
 					if ($cfg = file("{$phpbb_root_path}styles/$file/$subpath$mode.cfg"))
 					{
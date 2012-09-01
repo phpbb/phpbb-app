michaelcullum/phpbb

[ticket/10631] Validation for extensions
PHPBB3-10631

phpBB/includes/extension/manager.php
@@ -23,6 +23,7 @@ if (!defined('IN_PHPBB'))
 class phpbb_extension_manager
 {
 	protected $db;
+	protected $config;
 	protected $cache;
 	protected $phpEx;
 	protected $extensions;
@@ -34,16 +35,18 @@ class phpbb_extension_manager
 	* Creates a manager and loads information from database
 	*
 	* @param dbal $db A database connection
+	* @param phpbb_config $config phpbb_config
 	* @param string $extension_table The name of the table holding extensions
 	* @param string $phpbb_root_path Path to the phpbb includes directory.
 	* @param string $phpEx php file extension
 	* @param phpbb_cache_driver_interface $cache A cache instance or null
 	* @param string $cache_name The name of the cache variable, defaults to _ext
 	*/
-	public function __construct(dbal $db, $extension_table, $phpbb_root_path, $phpEx = '.php', phpbb_cache_driver_interface $cache = null, $cache_name = '_ext')
+	public function __construct(dbal $db, phpbb_config $config, $extension_table, $phpbb_root_path, $phpEx = '.php', phpbb_cache_driver_interface $cache = null, $cache_name = '_ext')
 	{
 		$this->phpbb_root_path = $phpbb_root_path;
 		$this->db = $db;
+		$this->config = $config;
 		$this->cache = $cache;
 		$this->phpEx = $phpEx;
 		$this->extension_table = $extension_table;
@@ -130,7 +133,7 @@ class phpbb_extension_manager
 	*/
 	public function get_extension_metadata($name, phpbb_template $template)
 	{
-		return new phpbb_extension_metadata_manager($name, $this->db, $this, $this->phpbb_root_path, $this->phpEx, $template);
+		return new phpbb_extension_metadata_manager($name, $this->db, $this, $this->phpbb_root_path, $this->phpEx, $template, $this->config);
 	}
 
 	/**
@@ -445,7 +448,7 @@ class phpbb_extension_manager
 		}
 		return $disabled;
 	}
-	
+
 	/**
 	* Check to see if a given extension is available on the filesystem
 	*

phpbb/phpbb-app

includes/extension/manager.php
@@ -23,6 +23,7 @@ if (!defined('IN_PHPBB'))
 class phpbb_extension_manager
 {
 	protected $db;
+	protected $config;
 	protected $cache;
 	protected $php_ext;
 	protected $extensions;
@@ -34,16 +35,18 @@ class phpbb_extension_manager
 	* Creates a manager and loads information from database
 	*
 	* @param dbal $db A database connection
+	* @param phpbb_config $config phpbb_config
 	* @param string $extension_table The name of the table holding extensions
 	* @param string $phpbb_root_path Path to the phpbb includes directory.
 	* @param string $php_ext php file extension
 	* @param phpbb_cache_driver_interface $cache A cache instance or null
 	* @param string $cache_name The name of the cache variable, defaults to _ext
 	*/
-	public function __construct(dbal $db, $extension_table, $phpbb_root_path, $php_ext = '.php', phpbb_cache_driver_interface $cache = null, $cache_name = '_ext')
+	public function __construct(dbal $db, phpbb_config $config, $extension_table, $phpbb_root_path, $php_ext = '.php', phpbb_cache_driver_interface $cache = null, $cache_name = '_ext')
 	{
 		$this->phpbb_root_path = $phpbb_root_path;
 		$this->db = $db;
+		$this->config = $config;
 		$this->cache = $cache;
 		$this->php_ext = $php_ext;
 		$this->extension_table = $extension_table;
@@ -130,7 +133,7 @@ class phpbb_extension_manager
 	*/
 	public function get_extension_metadata($name, phpbb_template $template)
 	{
-		return new phpbb_extension_metadata_manager($name, $this->db, $this, $this->phpbb_root_path, $this->phpEx, $template);
+		return new phpbb_extension_metadata_manager($name, $this->db, $this, $this->phpbb_root_path, $this->php_ext, $template, $this->config);
 	}
 
 	/**
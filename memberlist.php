Senky/phpbb

[ticket/12591] Improve breadcrumb naming and extend it's usage
PHPBB3-12591

phpBB/memberlist.php
@@ -364,6 +364,11 @@ switch ($mode)
 			}
 		}
 
+		$template->assign_block_vars('navlinks', array(
+			'BREADCRUMB_NAME'	=> $user->lang('THE_TEAM'),
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=team"),
+		));
+
 		$template->assign_vars(array(
 			'PM_IMG'		=> $user->img('icon_contact_pm', $user->lang['SEND_PRIVATE_MESSAGE']))
 		);
@@ -460,6 +465,11 @@ switch ($mode)
 				}
 			break;
 		}
+
+		$template->assign_block_vars('navlinks', array(
+			'BREADCRUMB_NAME'	=> $user->lang('IM_USER'),
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=contact&amp;action=$action"),
+		));
 
 		// Send vars to the template
 		$template->assign_vars(array(
@@ -784,6 +794,15 @@ switch ($mode)
 			);
 		}
 
+		$template->assign_block_vars('navlinks', array(
+			'BREADCRUMB_NAME'	=> $user->lang('MEMBERLIST'),
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx"),
+		));
+		$template->assign_block_vars('navlinks', array(
+			'BREADCRUMB_NAME'	=> $member['username'],
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=viewprofile&amp;u=$user_id"),
+		));
+
 		// Now generate page title
 		$page_title = sprintf($user->lang['VIEWING_PROFILE'], $member['username']);
 		$template_html = 'memberlist_view.html';
@@ -832,6 +851,20 @@ switch ($mode)
 			$messenger = new messenger(false);
 			$form->submit($messenger);
 		}
+
+		$u_breadcrumb = append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=$mode");
+		if ($user_id)
+		{
+			$u_breadcrumb .= '&amp;u=' . $user_id;
+		}
+		if ($topic_id)
+		{
+			$u_breadcrumb .= '&amp;t=' . $topic_id;
+		}
+		$template->assign_block_vars('navlinks', array(
+			'BREADCRUMB_NAME'	=> $form->get_page_title(),
+			'U_BREADCRUMB'		=> $u_breadcrumb,
+		));
 
 		$page_title = $form->get_page_title();
 		$template_html = $form->get_template_file();
@@ -1620,6 +1653,18 @@ switch ($mode)
 		extract($phpbb_dispatcher->trigger_event('core.memberlist_modify_template_vars', compact($vars)));
 
 		$template->assign_vars($template_vars);
+
+		$template->assign_block_vars('navlinks', array(
+			'BREADCRUMB_NAME'	=> $user->lang('MEMBERLIST'),
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx"),
+		));
+		if ($mode == 'group')
+		{
+			$template->assign_block_vars('navlinks', array(
+				'BREADCRUMB_NAME'	=> $group_helper->get_name($group_row['group_name']),
+				'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=group&amp;g=$group_id"),
+			));
+		}
 }
 
 // Output the page

phpBB/phpbb/help/controller/faq.php
@@ -160,6 +160,11 @@ class faq extends controller
 			)
 		);
 
+		$this->template->assign_block_vars('navlinks', array(
+			'BREADCRUMB_NAME'	=> $this->language->lang('FAQ'),
+			'U_BREADCRUMB'		=> $this->helper->route('phpbb_help_faq_controller'),
+		));
+
 		return $this->language->lang('FAQ_EXPLAIN');
 	}
 }

phpbb/phpbb-app

memberlist.php
@@ -365,8 +365,8 @@ switch ($mode)
 		}
 
 		$template->assign_block_vars('navlinks', array(
-			'FORUM_NAME'	=> $page_title,
-			'U_VIEW_FORUM'	=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=team"),
+			'BREADCRUMB_NAME'	=> $page_title,
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=team"),
 		));
 
 		$template->assign_vars(array(
@@ -467,8 +467,8 @@ switch ($mode)
 		}
 
 		$template->assign_block_vars('navlinks', array(
-			'FORUM_NAME'	=> $page_title,
-			'U_VIEW_FORUM'	=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=contact&amp;action=$action&amp;u=$user_id"),
+			'BREADCRUMB_NAME'	=> $page_title,
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=contact&amp;action=$action&amp;u=$user_id"),
 		));
 
 		// Send vars to the template
@@ -876,12 +876,12 @@ switch ($mode)
 		$template_html = 'memberlist_view.html';
 
 		$template->assign_block_vars('navlinks', array(
-			'FORUM_NAME'	=> $user->lang('MEMBERLIST'),
-			'U_VIEW_FORUM'	=> append_sid("{$phpbb_root_path}memberlist.$phpEx"),
+			'BREADCRUMB_NAME'	=> $user->lang('MEMBERLIST'),
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx"),
 		));
 		$template->assign_block_vars('navlinks', array(
-			'FORUM_NAME'	=> $member['username'],
-			'U_VIEW_FORUM'	=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=viewprofile&u=$user_id"),
+			'BREADCRUMB_NAME'	=> $member['username'],
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=viewprofile&u=$user_id"),
 		));
 
 	break;
@@ -950,8 +950,8 @@ switch ($mode)
 
 			generate_forum_nav($topic_data);
 			$template->assign_block_vars('navlinks', array(
-				'FORUM_NAME'	=> $topic_data['topic_title'],
-				'U_VIEW_FORUM'	=> append_sid("{$phpbb_root_path}viewtopic.$phpEx", "t=$topic_id"),
+				'BREADCRUMB_NAME'	=> $topic_data['topic_title'],
+				'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}viewtopic.$phpEx", "t=$topic_id"),
 			));
 
 			$navlink_name = $user->lang('EMAIL_TOPIC');
@@ -964,8 +964,8 @@ switch ($mode)
 		}
 
 		$template->assign_block_vars('navlinks', array(
-			'FORUM_NAME'	=> $navlink_name,
-			'U_VIEW_FORUM'	=> $navlink_url,
+			'BREADCRUMB_NAME'	=> $navlink_name,
+			'U_BREADCRUMB'		=> $navlink_url,
 		));
 
 	break;
@@ -1006,8 +1006,8 @@ switch ($mode)
 		$template_html = 'memberlist_body.html';
 
 		$template->assign_block_vars('navlinks', array(
-			'FORUM_NAME'	=> $page_title,
-			'U_VIEW_FORUM'	=> append_sid("{$phpbb_root_path}memberlist.$phpEx"),
+			'BREADCRUMB_NAME'	=> $page_title,
+			'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx"),
 		));
 
 		/* @var $pagination \phpbb\pagination */
@@ -1316,8 +1316,8 @@ switch ($mode)
 			}
 
 			$template->assign_block_vars('navlinks', array(
-				'FORUM_NAME'	=> $group_helper->get_name($group_row['group_name']),
-				'U_VIEW_FORUM'	=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=group&amp;g=$group_id"),
+				'BREADCRUMB_NAME'	=> $group_helper->get_name($group_row['group_name']),
+				'U_BREADCRUMB'		=> append_sid("{$phpbb_root_path}memberlist.$phpEx", "mode=group&amp;g=$group_id"),
 			));
 
 			$template->assign_vars(array(

phpbb/help/controller/faq.php
@@ -26,8 +26,8 @@ class faq extends controller
 		$this->language->add_lang('help/faq');
 
 		$this->template->assign_block_vars('navlinks', array(
-			'FORUM_NAME'	=> $this->language->lang('FAQ_EXPLAIN'),
-			'U_VIEW_FORUM'	=> $this->helper->route('phpbb_help_faq_controller'),
+			'BREADCRUMB_NAME'	=> $this->language->lang('FAQ_EXPLAIN'),
+			'U_BREADCRUMB'		=> $this->helper->route('phpbb_help_faq_controller'),
 		));
 
 		$this->manager->add_block(
1877c3319de8030a253bc1c92be8c4401568d26a
Zorglube <630192+zorglube@users.noreply.github.com>
2024-10-31 13:50:53 +0100

    Update config.php comment all

--- config.php
+++ config.php
@@ -1,33 +1,33 @@
-<?php
+// <?php
+// 
+// $dbms = 'phpbb\\db\\driver\\' . getenv('PHPBB_DB_DRIVER'); // PHPBB_DB_DRIVER="..."
+// $dbhost = getenv('POSTGRESQL_ADDON_HOST'); // PHPBB_DB_HOST="${POSTGRESQL_ADDON_HOST}"
+// $dbport = getenv('POSTGRESQL_ADDON_PORT'); // PHPBB_DB_PORT="${POSTGRESQL_ADDON_PORT}"
+// $dbname = getenv('POSTGRESQL_ADDON_DB'); // PHPBB_DB_NAME="${POSTGRESQL_ADDON_DB}"
+// $dbuser = getenv('POSTGRESQL_ADDON_USER'); // PHPBB_DB_USER="${POSTGRESQL_ADDON_USER}"
+// $dbpasswd = getenv('POSTGRESQL_ADDON_PASSWORD'); // PHPBB_DB_PASSWD="${POSTGRESQL_ADDON_PASSWORD}"
+// $table_prefix = getenv('PHPBB_DB_TABLE_PREFIX'); // PHPBB_DB_TABLE_PREFIX="..."
+// $phpbb_adm_relative_path = 'adm/';
+// $acm_type = 'phpbb\\cache\\driver\\file';
 
-$dbms = 'phpbb\\db\\driver\\' . getenv('PHPBB_DB_DRIVER'); // PHPBB_DB_DRIVER="..."
-$dbhost = getenv('POSTGRESQL_ADDON_HOST'); // PHPBB_DB_HOST="${POSTGRESQL_ADDON_HOST}"
-$dbport = getenv('POSTGRESQL_ADDON_PORT'); // PHPBB_DB_PORT="${POSTGRESQL_ADDON_PORT}"
-$dbname = getenv('POSTGRESQL_ADDON_DB'); // PHPBB_DB_NAME="${POSTGRESQL_ADDON_DB}"
-$dbuser = getenv('POSTGRESQL_ADDON_USER'); // PHPBB_DB_USER="${POSTGRESQL_ADDON_USER}"
-$dbpasswd = getenv('POSTGRESQL_ADDON_PASSWORD'); // PHPBB_DB_PASSWD="${POSTGRESQL_ADDON_PASSWORD}"
-$table_prefix = getenv('PHPBB_DB_TABLE_PREFIX'); // PHPBB_DB_TABLE_PREFIX="..."
-$phpbb_adm_relative_path = 'adm/';
-$acm_type = 'phpbb\\cache\\driver\\file';
+// if (getenv('PHPBB_IS_INSTALLED') === 'true') {
+// 	@define('PHPBB_INSTALLED', true);
+// }
 
-if (getenv('PHPBB_IS_INSTALLED') === 'true') {
-	@define('PHPBB_INSTALLED', true);
-}
+// if (getenv('PHPBB_DISPLAY_LOAD_TIME') === 'true') {
+// 	@define('PHPBB_DISPLAY_LOAD_TIME', true);
+// }
 
-if (getenv('PHPBB_DISPLAY_LOAD_TIME') === 'true') {
-	@define('PHPBB_DISPLAY_LOAD_TIME', true);
-}
+// if (getenv('PHPBB_DEBUG') === 'true') {
+// 	@define('DEBUG', true);
+// }
 
-if (getenv('PHPBB_DEBUG') === 'true') {
-	@define('DEBUG', true);
-}
+// if (getenv('PHPBB_DEBUG_CONTAINER') === 'true') {
+// 	@define('DEBUG_CONTAINER', true);
+// }
 
-if (getenv('PHPBB_DEBUG_CONTAINER') === 'true') {
-	@define('DEBUG_CONTAINER', true);
-}
-
-if (is_null(getenv('PHPBB_ENVIRONMENT'))) {
-	@define('PHPBB_ENVIRONMENT', 'production');
-} else {
-	@define('PHPBB_ENVIRONMENT', getenv('PHPBB_ENVIRONMENT'));
-}
+// if (is_null(getenv('PHPBB_ENVIRONMENT'))) {
+// 	@define('PHPBB_ENVIRONMENT', 'production');
+// } else {
+// 	@define('PHPBB_ENVIRONMENT', getenv('PHPBB_ENVIRONMENT'));
+// }
